3afaf5e91c3a952e6cfc5ea794033137b2b915b3
Muh Ali Bakhtiar <muhalibakhtiar@gmail.com>
2024-05-02 21:27:49 +0700

    add api

--- services/main.py
+++ services/main.py
@@ -1,6 +1,17 @@
 from typing import Union
+import requests
+import json
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+
+
+class DataDiri(BaseModel):
+    name : str
+    age : int
+    asal : str
+
 
 app = FastAPI()
 
@@ -10,9 +21,20 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/namaku/{name}") # path parameter
+def read_item(name: str):
+    print(name)
+    return {"message": f"nama saya adalah {name}"}
+
+
+@app.get("/query-paramter/{name}") # query parameter
+def read_item(name: str, age:int, asal:str):
+    print(name)
+    return {"message": f"nama saya adalah {name} usia saat ini adalah {age} dan saya berasal dari {asal}"}
+
+@app.post("/json-body") # body request
+def getJsonBody(request:DataDiri):
+    return request
 
 @app.get("/get-list-data-user")
 def read_root(
@@ -29,3 +51,12 @@ def read_root(
         }
     }
     return outres
+
+
+@app.get("/wilayah-provinsi")
+def getWilayahIndo():
+    url = "https://www.emsifa.com/api-wilayah-indonesia/api/provinces.json"
+    out_resp = requests.get(url)
+    print(out_resp)
+
+    return out_resp
